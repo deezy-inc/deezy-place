96134a36564125207f2ff532ae1563caf73f2d5e
Danny Diekroeger <dannydiek@gmail.com>
2023-01-31 04:48:25 -0800

    remove unused code

--- src/App.js
+++ src/App.js
@@ -3,38 +3,18 @@ import './App.css';
 
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Modal from 'react-bootstrap/Modal';
-import DeezyLogo from './assets/images/Deezy 2.svg';
 import { SocialIcon } from 'react-social-icons';
-var ReactRotatingText = require('react-rotating-text');
-import { FadeIn } from 'react-slide-fade-in';
-import NavLink from 'react-bootstrap/esm/NavLink';
-const axios = require('axios')
 
+const axios = require('axios')
 import * as bitcoin from 'bitcoinjs-lib'
 import * as ecc from 'tiny-secp256k1'
-import { BIP32Factory } from 'bip32'
-// You must wrap a tiny-secp256k1 compatible implementation
-const bip32 = BIP32Factory(ecc)
 bitcoin.initEccLib(ecc)
 
-import { nip19, generatePrivateKey, getPublicKey } from 'nostr-tools'
-
-const DISCORD_URL = "https://discord.gg/ASfVZ8Cc6f"
-const TWITTER_URL = "https://twitter.com/astral_babes"
-const NODE_ID = "024bfaf0cabe7f874fd33ebf7c6f4e5385971fc504ef3f492432e9e3ec77e1b5cf"
-
-
-
 const App = () => {
-  const exploreRef = useRef(null);
-  const scrollToRef = (ref) => ref.current.scrollIntoView({ block: 'start', behavior: 'smooth' });
-  const [showBeginMintModal, setShowBeginMintModal] = useState(false);
   const [nostrPublicKey, setNostrPublicKey] = useState(null);
   const [showReceiveAddressModal, setShowReceiveAddressModal] = useState(false);
   const [ownedUtxos, setOwnedUtxos] = useState([]);

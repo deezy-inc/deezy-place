a6a39df2ce9044e2f61c7f75eb02175fdf842223
Danny Diekroeger <dannydiek@gmail.com>
2023-02-17 17:27:54 -0800

    tx bytes

--- src/constance.js
+++ src/constance.js
@@ -3,4 +3,4 @@ export const GITHUB_URL = "https://github.com/dannydeezy/nosft"
 export const DEFAULT_FEE_RATE = 7
 export const SENDS_ENABLED = true
 export const TESTNET = false
-export const ASSUMED_TX_BYTES = 41 + 10 + 43
+export const ASSUMED_TX_BYTES = 111

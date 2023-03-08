cbf7941752cdc7fa5631251bed9d91f1e5469cf2
topether21 <topether21@gmail.com>
2023-03-08 15:52:28 -0600

    chore: fix funky header on scroll

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,12 @@
 /* eslint-disable no-restricted-syntax, no-await-in-loop, no-continue */
 
-import React, { useState, useEffect, useMemo } from "react";
-import * as bitcoin from "bitcoinjs-lib";
+import React, { useState, useEffect, useMemo, useRef } from "react";
 import Wrapper from "@layout/wrapper";
 import Header from "@layout/header";
 import Footer from "@layout/footer";
 import SEO from "@components/seo";
 import HeroArea from "@containers/HeroArea";
 import OrdinalsArea from "@containers/OrdinalsArea";
-import OnSaleOrdinalsArea from "@containers/OnSaleOrdinalsArea";
 import { normalizedData, getQueryStringParam } from "@utils/methods";
 import { getAddressInfo } from "@utils/crypto";
 import homepageData from "@data/general/home.json";
@@ -21,6 +19,8 @@ export async function getStaticProps() {
 
 const App = () => {
     const [isExperimental, setIsExperimental] = useState(false);
+    const [headerHeight, setHeaderHeight] = useState(148); // Optimistically
+    const elementRef = useRef(null);
 
     const [nostrAddress, setNostrAddress] = useState();
     const { nostrPublicKey, onConnectHandler, onDisconnectHandler } = useConnectWallet();
@@ -29,6 +29,9 @@ const App = () => {
         const exp = getQueryStringParam("__mode");
         if (exp === "astral") {
             setIsExperimental(true);
+        }
+        if (elementRef.current) {
+            setHeaderHeight(elementRef.current.clientHeight);
         }
     }, []);
 
@@ -55,13 +58,13 @@ const App = () => {
             <Wrapper>
                 <SEO pageTitle="Deezy" />
                 <Header
+                    ref={elementRef}
                     nostrPublicKey={nostrPublicKey}
                     onConnectHandler={onConnectHandler}
                     onDisconnectHandler={onDisconnectHandler}
                     address={nostrAddress}
                 />
-
-                <main id="main-content">
+                <main id="main-content" style={{ paddingTop: headerHeight }}>
                     {!nostrPublicKey && <HeroArea data={content["hero-section"]} onConnectHandler={onConnectHandler} />}
 
                     {nostrPublicKey && nostrAddress && <OrdinalsArea />}

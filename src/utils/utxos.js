eff773b805a98f356076a89ed7b6b3a3d39ef995
Danny Diekroeger <dannydiek@gmail.com>
2023-03-11 13:23:41 -0800

    Use blockstream, put delays in

--- src/utils/utxos.js
+++ src/utils/utxos.js
@@ -4,7 +4,8 @@ const axios = require("axios");
 
 // eslint-disable-next-line no-promise-executor-return
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-const baseMempoolUrl = TESTNET ? "https://mempool.space/signet" : "https://mempool.space";
+// We use blockstream.info for mainnet because it has a higher rate limit than mempool.space (same API).
+const baseMempoolUrl = TESTNET ? "https://mempool.space/signet" : "https://blockstream.info";
 export const getAddressUtxos = async (address) => {
     console.log(`Getting address utxos`);
     let utxos;
@@ -13,6 +14,7 @@ export const getAddressUtxos = async (address) => {
         const { data } = await axios.get(`${baseMempoolUrl}/api/address/${address}/utxo`);
         utxos = data;
     } catch (err) {
+        await delay(100);
         // Some addresses have too many utxos and mempool.space throws an erorr. In this case we need to manually
         // search through all transactions, find the outputs, and then remove the outputs that have been spent.
         console.log(`Failed to fetch utxos.. assuming there are too many to fetch. Will use pagination`);
@@ -21,10 +23,24 @@ export const getAddressUtxos = async (address) => {
         let lastSeenTxId = null;
         // We do one pass through to find all outputs and spent outputs.
         while (true) {
+            // Short delay to help get around rate limits.
+            // eslint-disable-next-line no-await-in-loop
+            console.log(lastSeenTxId);
             const url = `${baseMempoolUrl}/api/address/${address}/txs${lastSeenTxId ? `/chain/${lastSeenTxId}` : ""}`;
             // eslint-disable-next-line no-await-in-loop
-            const { data } = await axios.get(url);
-            const txs = data;
+            let resp;
+            try {
+                // eslint-disable-next-line no-await-in-loop
+                resp = await axios.get(url);
+            } catch (e) {
+                console.log(`Error`);
+                console.error(e);
+                // eslint-disable-next-line no-await-in-loop
+                await delay(5000);
+                // eslint-disable-next-line no-continue
+                continue;
+            }
+            const txs = resp.data;
             if (txs.length === 0) break;
             // eslint-disable-next-line no-restricted-syntax
             for (const tx of txs) {
@@ -45,9 +61,6 @@ export const getAddressUtxos = async (address) => {
                 }
             }
             lastSeenTxId = txs[txs.length - 1].txid;
-            // Short delay to help get around rate limits.
-            // eslint-disable-next-line no-await-in-loop
-            await delay(50);
         }
         // Now we filter out all outputs that have been spent.
         utxos = outputs.filter((it) => !spentOutpoints.has(`${it.txid}:${it.vout}`));

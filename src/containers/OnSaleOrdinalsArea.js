5b99079d9be2e2c390ce73d80a2f008f1d9d00a6
topether21 <topether21@gmail.com>
2023-03-12 20:01:14 -0600

    chore: improve function names

--- src/containers/OnSaleOrdinalsArea.js
+++ src/containers/OnSaleOrdinalsArea.js
@@ -1,13 +1,13 @@
 /* eslint-disable react/forbid-prop-types */
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable no-extra-boolean-cast */
-import { useContext, useState, useEffect, useCallback } from "react";
+import { useContext, useState, useEffect, useCallback, useRef } from "react";
 import PropTypes from "prop-types";
 import clsx from "clsx";
 import SectionTitle from "@components/section-title";
 import OrdinalCard from "@components/ordinal-card";
 import { deepClone } from "@utils/methods";
-import { OpenOrdex } from "@utils/openOrdexV3";
+// import { OpenOrdex } from "@utils/openOrdexV3";
 import WalletContext from "@context/wallet-context";
 import { nostrPool } from "@services/nostr-relay";
 import { MAX_ONSALE } from "@lib/constants.config";
@@ -27,27 +27,16 @@ const collectionAuthor = [
 const OnSaleOrdinalsArea = ({ className, space, onConnectHandler, onSale }) => {
     const { nostrAddress, isExperimental } = useContext(WalletContext);
     const [openOrders, setOpenOrders] = useState([]);
-    const [isLoadingOpenOrders, setIsLoadingOpenOrders] = useState(true);
-    const addOrdinals$ = new Subject();
+    const [isLoadingOpenOrders] = useState(false); // it is necessary?
+    const addOpenOrder$ = useRef(new Subject());
+    const addSubscriptionRef = useRef(null);
+    const orderSubscriptionRef = useRef(null);
 
-    addOrdinals$
-        .pipe(
-            scan((acc, curr) => {
-                // We can add only unique ordinals
-                if (acc.find((order) => order.inscriptionId === curr.inscriptionId)) {
-                    return acc;
-                }
-                // We sort by created_at DESC and limit list
-                return [...acc, curr].sort((a, b) => b.created_at - a.created_at).slice(0, MAX_ONSALE);
-            }, openOrders)
-        )
-        .subscribe(setOpenOrders);
-
-    const addNewOrdinal = (ordinal) => {
-        addOrdinals$.next(ordinal);
+    const addNewOpenOrder = (order) => {
+        addOpenOrder$.current.next(order);
     };
 
-    const formatOrdinal = useCallback((inscription) => {
+    const formatOrder = useCallback((inscription) => {
         const inscriptionData = Object.assign(
             {},
             ...inscription.tags
@@ -65,32 +54,50 @@ const OnSaleOrdinalsArea = ({ className, space, onConnectHandler, onSale }) => {
     }, []);
 
     useEffect(() => {
-        console.log("OnSaleOrdinalsArea useEffect");
-        let latestOrderSubscription;
-        let orderSubscription;
-        const load = async () => {
-            console.log("loading on sale ordinals");
-            const openOrderx = await OpenOrdex.init();
-            latestOrderSubscription = openOrderx.latestOrders({ limit: 1000 }).subscribe((order) => {
-                const formattedOrdinal = formatOrdinal(order);
-                console.log("from latestOrderSubscription", formattedOrdinal.inscriptionId);
-                addNewOrdinal(formattedOrdinal);
-            });
-            orderSubscription = nostrPool.subscribeOriginals({ limit: MAX_ONSALE }).subscribe((order) => {
-                const formattedOrdinal = formatOrdinal(order);
-                console.log("from orderSubscription", formattedOrdinal.inscriptionId);
-                addNewOrdinal(formatOrdinal(order));
-            });
-            setIsLoadingOpenOrders(false);
-        };
-        load();
+        console.log("stream orders");
+        addSubscriptionRef.current = addOpenOrder$.current
+            .pipe(
+                scan((acc, curr) => {
+                    // We can add only unique ordinals
+                    if (acc.find((order) => order.id === curr.id)) {
+                        return acc;
+                    }
+                    // We sort by created_at DESC and limit list
+                    return [...acc, curr].sort((a, b) => b.created_at - a.created_at).slice(0, MAX_ONSALE);
+                }, openOrders)
+            )
+            .subscribe(setOpenOrders);
+        orderSubscriptionRef.current = nostrPool.subscribeOrders({ limit: MAX_ONSALE }).subscribe((order) => {
+            const formattedOrder = formatOrder(order);
+            console.log("from orderSubscription", formattedOrder.inscriptionId);
+            addNewOpenOrder(formatOrder(order));
+        });
         return () => {
-            console.log("unsubscribe");
-            latestOrderSubscription?.unsubscribe();
-            orderSubscription?.unsubscribe();
+            orderSubscriptionRef.current.unsubscribe();
+            addSubscriptionRef.current.unsubscribe();
         };
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
+
+    // TODO: it is really necessary to fetch with openOrderx? with nostrPool we get the same.
+    // const latestOrderSubscriptionRef = useRef(null);
+    // useEffect(() => {
+    //     const load = async () => {
+    //         console.log("loading on sale ordinals");
+    //         const openOrderx = await OpenOrdex.init();
+    //         latestOrderSubscriptionRef.current = openOrderx.latestOrders({ limit: MAX_ONSALE }).subscribe((order) => {
+    //             const formattedOrder = formatOrder(order);
+    //             console.log("from latestOrderSubscription", formattedOrder.inscriptionId);
+    //             addNewOpenOrder(formattedOrder);
+    //         });
+    //         setIsLoadingOpenOrders(false);
+    //     };
+    //     load();
+    //     return () => {
+    //         latestOrderSubscriptionRef.current.unsubscribe();
+    //     };
+    //     // eslint-disable-next-line react-hooks/exhaustive-deps
+    // }, []);
 
     return (
         <div id="selling-collection" className={clsx("rn-product-area", space === 1 && "rn-section-gapTop", className)}>

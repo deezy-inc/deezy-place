8b8aa3ba445c6a703b773626d43fcd3836af7bfd
andredevjs <97211514+andredevjs@users.noreply.github.com>
2023-04-17 11:38:39 -0600

    fix: build

--- src/utils/psbt.js
+++ src/utils/psbt.js
@@ -10,7 +10,6 @@ import * as ecc from "tiny-secp256k1";
 import SessionStorage, { SessionsStorageKeys } from "@services/session-storage";
 import axios from "axios";
 
-
 bitcoin.initEccLib(ecc);
 
 const ECPair = ECPairFactory(ecc);

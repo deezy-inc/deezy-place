8447bf384bda90346438609892d21eb5d976f111
andredevjs <97211514+andredevjs@users.noreply.github.com>
2023-04-24 07:46:27 -0600

    chore: whole card clickable

--- src/components/ordinal-card/index.js
+++ src/components/ordinal-card/index.js
@@ -23,59 +23,63 @@ const OrdinalCard = ({ overlay, price, type, utxo, authors, confirmed, date, onS
 
     return (
         <SkeletonTheme baseColor="#13131d" highlightColor="#242435">
-            <div className={clsx("product-style-one", !overlay && "no-overlay")}>
-                <div className="card-thumbnail">
-                    <InscriptionPreview utxo={utxo} />
-                </div>
-                <div className="inscription-details-area">
-                    {utxo && <div className="inscription-number">{utxo.inscriptionId ? `#${utxo.num}` : "\u00A0"}</div>}
-                    {!utxo && <Skeleton width={50} />}
-                </div>
-                <div className="product-share-wrapper">
-                    <div className="profile-share">
-                        {authors?.map((client) => (
-                            <ClientAvatar
-                                key={client.name}
-                                slug={client.slug}
-                                name={utxo.inscriptionId || utxo.txid}
-                                image={client.image}
-                            />
-                        ))}
-                        {!authors && <Skeleton circle height={40} width={40} />}
-                        <div className="more-author-text">
-                            {utxo && Boolean(utxo.inscriptionId) && (
-                                <Anchor className="logo-dark" path={`/inscription/${utxo.inscriptionId}`}>
-                                    {shortenStr(utxo.inscriptionId)}
-                                </Anchor>
-                            )}
-                            {(!utxo || !utxo.inscriptionId) && <Skeleton width={140} />}
-                        </div>
+            <Anchor className="logo-dark" path={utxo?.inscriptionId ? `/inscription/${utxo?.inscriptionId}` : "#"}>
+                <div className={clsx("product-style-one", !overlay && "no-overlay")}>
+                    <div className="card-thumbnail">
+                        <InscriptionPreview utxo={utxo} />
                     </div>
-                    {nostrAddress && utxo && type !== "send" && type !== "buy" && (
-                        <CardOptions utxo={utxo} onSale={onSale} />
-                    )}
-                </div>
-                {/* <Anchor path={`#${slug}`}>
+                    <div className="inscription-details-area">
+                        {utxo && (
+                            <div className="inscription-number">{utxo.inscriptionId ? `#${utxo.num}` : "\u00A0"}</div>
+                        )}
+                        {!utxo && <Skeleton width={50} />}
+                    </div>
+                    <div className="product-share-wrapper">
+                        <div className="profile-share">
+                            {authors?.map((client) => (
+                                <ClientAvatar
+                                    key={client.name}
+                                    slug={client.slug}
+                                    name={utxo.inscriptionId || utxo.txid}
+                                    image={client.image}
+                                />
+                            ))}
+                            {!authors && <Skeleton circle height={40} width={40} />}
+                            <div className="more-author-text">
+                                {utxo && Boolean(utxo.inscriptionId) && (
+                                    <Anchor className="logo-dark" path={`/inscription/${utxo.inscriptionId}`}>
+                                        {shortenStr(utxo.inscriptionId)}
+                                    </Anchor>
+                                )}
+                                {(!utxo || !utxo.inscriptionId) && <Skeleton width={140} />}
+                            </div>
+                        </div>
+                        {nostrAddress && utxo && type !== "send" && type !== "buy" && (
+                            <CardOptions utxo={utxo} onSale={onSale} />
+                        )}
+                    </div>
+                    {/* <Anchor path={`#${slug}`}>
                     <span className="product-name">{title}</span>
                 </Anchor> */}
-                {/* {description && <span className="latest-bid">{description}</span>} */}
-                {/* {!description && <Skeleton width={200} />} */}
-                {utxo && (
-                    <ProductBid
-                        price={price}
-                        utxo={utxo}
-                        confirmed={confirmed}
-                        date={date}
-                        type={type}
-                        onSale={onSale}
-                    />
-                )}
-                {!utxo && (
-                    <div className="mt--10">
-                        <Skeleton width={200} count={2} />
-                    </div>
-                )}
-            </div>
+                    {/* {description && <span className="latest-bid">{description}</span>} */}
+                    {/* {!description && <Skeleton width={200} />} */}
+                    {utxo && (
+                        <ProductBid
+                            price={price}
+                            utxo={utxo}
+                            confirmed={confirmed}
+                            date={date}
+                            type={type}
+                            onSale={onSale}
+                        />
+                    )}
+                    {!utxo && (
+                        <div className="mt--10">
+                            <Skeleton width={200} count={2} />
+                        </div>
+                    )}
+                </div>
+            </Anchor>
         </SkeletonTheme>
     );
 };

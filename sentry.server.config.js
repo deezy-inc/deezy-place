170a4bdfc2e9489396791b954c1bdc1e6b46cfc9
habibitcoin <hi@habibitcoin.com>
2023-07-17 17:12:25 -0700

    Add Sentry.io to next server MVP

---
+++ sentry.server.config.js
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://1b2decb65870413bb10959f3eb1d227b@o4505546397712384.ingest.sentry.io/4505546453811200",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});

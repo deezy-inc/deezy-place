d38fa7ff800560e34b7933ee463a6c9e5d539d0e
andredevjs <97211514+andredevjs@users.noreply.github.com>
2023-07-27 20:34:34 -0600

    feat: sentry

--- next.config.js
+++ next.config.js
@@ -50,6 +50,7 @@ module.exports = withSentryConfig(
     // Suppresses source map uploading logs during build
     silent: true,
 
+    authToken: process.env.SENTRY_AUTH_TOKEN,
     org: "deezy-io",
     project: "deezy-place",
   },
